3ccfc4b4b465b422bf6fea934c3391ef9b411327
nick.gnmc <nick.sutton@gbmc.ac.uk>
2025-09-09 14:59:15 +0100

    Working code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-alert("Hello world");
+$("#calcVAT").click(function () {
+    let vat = parseFloat($("#amount").val()) *0.2;
+    $("#vat").text(vat.toFixed(2));
+    for (let i = 1; i <= 10; i++) {
+        $("#vat")
+            .fadeOut(300)
+            .fadeIn(300);
+        }
+})
+
+$("#calcVAT").hover(function () {
+    $(this).css("color", "red");
+    $(this).css("width", "200px");
+})
+
+$("#calcVAT").mouseleave(function () {
+    $(this).css("color", "black");
+     $(this).css("width", "150px");
+})
